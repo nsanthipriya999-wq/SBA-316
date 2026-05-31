607d5634869fd333c6166005ec769fc6260fc302
santhi999 <nsanthipriya999@gmail.com>
2026-05-31 10:06:14 -0400

    second commit

--- script.js
+++ script.js
@@ -4,27 +4,29 @@ const attemptsText=document.getElementById("attempts");
 const cluesText=document.getElementById("clues");
 const message=document.getElementById("message");
 const code=document.getElementById("secretCode");
+const timerText=document.getElementById("timer");
 //--------------Variables-----------
 let firstCard=null;
 let secondCard=null;
 let lock=false;
 let attemptsDone=0;
 let cluesFound=0;
-
+let time=0;
+let timerInterval=null;
 //--------------secret code---------------
 const secretCode="98435267"
 
 
         //clue list
          const cluesList=[ 
-                  "Digit 1=9",
-                  "Digit 2=8",
-                  "Digit 3=4",
-                  "Digit 4=3",
-                  "Digit 5=5",
-                  "Digit 6=2",
-                  "Digit 7=6",
-                  "Digit 8=7"
+                  "First Digit=9",
+                  "Second Digit=8",
+                  "Third Digit=4",
+                  "Fourth Digit =3",
+                  "Fifth Digit =5",
+                  "Sixth Digit =2",
+                  "Seventh Digit=6",
+                  "Eight Digit =7"
                 ];          
                   
 
@@ -64,6 +66,16 @@ function createBoard()
   board.appendChild(fragment);
 }
 
+//------Start Timer-------------
+function startTimer()
+{
+  timerInterval=setInterval(()=>{
+   time++;
+   timerText.textContent="Time:"+time+"s";
+
+  },1000);
+
+}
 //-------Flip Card--------------
 function flipCard()
 {
@@ -122,11 +134,7 @@ function checkMatch()
 //-----------Clues-----------------
 function revealClue()
 {
- 
-  
-  
-  
-    if(cluesFound<=cluesList.length)
+     if(cluesFound<=cluesList.length)
    {
     message.textContent=cluesList[cluesFound-1];
 
@@ -156,8 +164,10 @@ function checkWin()
   const matched=document.querySelectorAll(".matched");
 
   if(matched.length==16){
+    clearInterval(timerInterval);
     setTimeout(()=>{
        message.textContent="Congratulations🎉🎉!! All Clues Found! Now crack the secret code!";
+       alert("You completed the Brain Blitz:Mind Vault game!!")
 
     },300);
 
@@ -171,7 +181,7 @@ document.getElementById("submit").addEventListener("click",()=>{
 
     //pattern matching
     if(!/^\d{8}$/.test(guess)){
-       alert("Enter a valid 8 digit Code!");
+       alert("Enter a valid 8 digit Code, accepts only  numbers!");
        return;
 
     }
@@ -179,6 +189,7 @@ document.getElementById("submit").addEventListener("click",()=>{
     if(guess===secretCode){
         alert("ACCESS GRANTED 🔓");
         localStorage.setItem("BestAttempts",attemptsDone);
+        localStorage.setItem("BestTime:"+time);
     }
     else{
         alert("Wrong code! Please Try Again!")
@@ -193,9 +204,20 @@ document.getElementById("restart").addEventListener("click",()=>{
         //reset variables
         attemptsDone=0;
         cluesFound=0;
+        firstCard=null;
+        secondCard=null;
+        lock=false;
+
+        //reset timer
+
+        clearInterval(timerInterval);
+        time=0;
+        timerText.textContent="Time:0s";
+        startTimer();
         //reset ux
         attemptsText.textContent="Attempts:0";
         cluesText.textContent="Clues Found: 0";
+        message.textContent="Find Matches to reveal the code!"
 
         //clear board
         board.innerHTML="";
@@ -208,7 +230,7 @@ document.getElementById("restart").addEventListener("click",()=>{
 
 //-------------Start Game---------------------
 createBoard();
-
+startTimer();
 
 
 
